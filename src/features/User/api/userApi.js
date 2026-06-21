4e6d87659202e1236ac2eb91bdf413a8d2c64bd0
MohammadHaboosh <mohammadhaboosh25@gmail.com>
2026-06-21 18:15:36 +0300

    ..

--- src/features/User/api/userApi.js
+++ src/features/User/api/userApi.js
@@ -1,21 +1,20 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const registerUser = async (userData) => {
-  const response = await fetch(
-    `${import.meta.env.VITE_API_BASE_URL}/authentication/sign-up`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        firstName: userData.firstName,
-        lastName: userData.lastName,
-        imagePath: "123456789",
-        birthDate: userData.birthDate,
-        email: userData.email,
-        password: userData.password,
-      }),
+  const response = await fetch(`${BASE_URL}/authentication/sign-up`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-  );
+    body: JSON.stringify({
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      imagePath: "123456789",
+      birthDate: userData.birthDate,
+      email: userData.email,
+      password: userData.password,
+    }),
+  });
 
   const data = await response.json();
 
@@ -29,19 +28,16 @@ export const registerUser = async (userData) => {
 
 export const signinUser = async (credentials) => {
   try {
-    const response = await fetch(
-      `${import.meta.env.VITE_API_BASE_URL}/authentication/sign-in`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: credentials.email,
-          password: credentials.password,
-        }),
+    const response = await fetch(`${BASE_URL}/authentication/sign-in`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    );
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
 
     const data = await response.json();
 
