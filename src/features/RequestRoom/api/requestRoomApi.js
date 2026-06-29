6523e8647d5a7da3d27cae553265b4719d72d953
Yazan420 <yazanmahfooz8@gmail.com>
2026-06-29 17:44:24 +0300

    ..

--- src/features/RequestRoom/api/requestRoomApi.js
+++ src/features/RequestRoom/api/requestRoomApi.js
@@ -1,17 +1,18 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getUploadUrl = async (fileName) => {
-  console.log("fileName: ", fileName);
+  console.log('fileName: ', fileName);
   const response = await fetch(`${BASE_URL}/demos/upload-url`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      "x-client-type": "web",
+      'Content-Type': 'application/json',
+      'x-client-type': 'web',
     },
+    credentials: 'include',
     body: JSON.stringify({ fileName }),
   });
 
-  if (!response.ok) throw new Error("Failed to generate upload URL");
+  if (!response.ok) throw new Error('Failed to generate upload URL');
   return response.json();
 };
 
@@ -22,28 +23,28 @@ export const uploadFileToCloud = async (uploadUrl, fields, file) => {
     cloudFormData.append(key, fields[key]);
   });
 
-  cloudFormData.append("file", file);
+  cloudFormData.append('file', file);
 
   const response = await fetch(uploadUrl, {
-    method: "POST",
+    method: 'POST',
     body: cloudFormData,
   });
 
   if (!response.ok && response.status !== 204) {
-    throw new Error("Failed to upload image to the cloud");
+    throw new Error('Failed to upload image to the cloud');
   }
 };
 
 export const createRoom = async (data) => {
   const response = await fetch(`${BASE_URL}/demos`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      "x-client-type": "web",
+      'Content-Type': 'application/json',
+      'x-client-type': 'web',
     },
     body: JSON.stringify(data),
   });
 
-  if (!response.ok) throw new Error("Failed to create the room");
+  if (!response.ok) throw new Error('Failed to create the room');
   return response.json();
 };
