6eb65920fb2b218463a961a17568a26768664157
MohammadHaboosh <mohammadhaboosh25@gmail.com>
2026-06-29 14:46:31 +0300

    ..

--- src/features/RequestRoom/api/requestRoomApi.js
+++ src/features/RequestRoom/api/requestRoomApi.js
@@ -1,6 +1,7 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getUploadUrl = async (fileName) => {
+  console.log("fileName: ", fileName);
   const response = await fetch(`${BASE_URL}/demos/upload-url`, {
     method: "POST",
     headers: {
