e091ccaf2636f48f00a852781291ff378794d030
Abrar Abo Auad <abraraboauad2@gmail.com>
2026-06-30 00:58:02 +0300

    ..

--- src/components/elements/RoomCard/RoomCard.jsx
+++ src/components/elements/RoomCard/RoomCard.jsx
@@ -10,8 +10,8 @@ const RoomCard = ({ room }) => {
 
   const handleCardClick = () => {
     ////////////////////////////////////////
-    const tempDemoId = "019f0fe8-d807-73b9-a0c7-891a13776c4b";
-    navigate(`/demos/${tempDemoId}`);
+    /// const tempDemoId = ;
+    navigate(`/demos/019f0fe8-d807-73b9-a0c7-891a13776c4b`);
   };
 
   return (
