d80bb23116ee303416b45f9d5e620c3a71ea5610
Abrar Abo Auad <abraraboauad2@gmail.com>
2026-07-06 23:20:48 +0300

    fixes

--- src/routes/paths.js
+++ src/routes/paths.js
@@ -36,7 +36,8 @@ export const PATHS = {
   OWNER_MEMBERS: "members",
   OWNER_INQUIRIES: "inquiries",
   OWNER_LIBRARY: "library",
-  OwnerCoursesPage: "manage-courses",
+  OWNER_COURSES: "manage-courses",
+  PSD_COURSES: "Purchased courses",
 
   SM_ASSETS: "demo-assets",
   SM_MEMBERS: "members",
