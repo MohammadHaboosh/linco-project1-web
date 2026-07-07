2bf64d044c09b086a66cd81b6e5059f2da91828b
Abrar Abo Auad <abraraboauad2@gmail.com>
2026-07-07 04:52:47 +0300

    ..

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -109,21 +109,7 @@ export const router = createBrowserRouter([
     path: PATHS.DEMO_SECTION,
     element: <DepartmentLayout />,
     children: [
-      {
-        index: true,
-        element: <DepartmentPage />,
-        children: [
-          {
-            path: PATHS.COURSE_PLAYER,
-            element: <CoursePlayerPage />,
-            children: [
-              { index: true, element: <CoursePlayerPage /> },
-              { path: PATHS.LESSON, element: <VideoPlayer /> },
-              { path: PATHS.QUIZ, element: <QuizContainer /> },
-            ],
-          },
-        ],
-      },
+      { index: true, element: <DepartmentPage /> },
       {
         path: PATHS.LEARNING_PATH,
         element: <LearningPathPage />,
@@ -136,7 +122,15 @@ export const router = createBrowserRouter([
         path: PATHS.LIVES,
         element: <LivesPage />,
       },
-
+      {
+        path: PATHS.COURSE_PLAYER,
+        element: <CoursePlayerPage />,
+        children: [
+          { index: true, element: <CoursePlayerPage /> },
+          { path: PATHS.LESSON, element: <VideoPlayer /> },
+          { path: PATHS.QUIZ, element: <QuizContainer /> },
+        ],
+      },
       // {
       //   path: PATHS.WEEKLY_TASKS,
       //   element: <WeeklyTasksPage />,
