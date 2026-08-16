b441dcc07ebc711c54f07a68be51b8017d05c44b
Abrar Abo Auad <abraraboauad2@gmail.com>
2026-08-17 02:01:47 +0300

    fix conflict between plyr - hls.js

--- src/features/Demo/CoursePlayer/components/CourseViewer/VideoContent.jsx
+++ src/features/Demo/CoursePlayer/components/CourseViewer/VideoContent.jsx
@@ -227,12 +227,20 @@ const VideoContent = ({ activeLesson, onNext, onPrev }) => {
   const isHls = finalVideoUrl.includes(".m3u8");
   const videoSrc = {
     type: "video",
-    sources: [
-      {
-        src: finalVideoUrl,
-        type: isHls ? "application/x-mpegURL" : "video/mp4",
-      },
-    ],
+    sources:
+      isHls && Hls.isSupported()
+        ? [
+            {
+              src: "",
+              type: "video/mp4",
+            },
+          ]
+        : [
+            {
+              src: finalVideoUrl,
+              type: isHls ? "application/x-mpegURL" : "video/mp4",
+            },
+          ],
   };
 
   return (
