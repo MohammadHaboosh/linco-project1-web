1983e7e7a333f7c3153202fdf5d0b8e0e435e60f
Abrar Abo Auad <abraraboauad2@gmail.com>
2026-08-20 00:54:14 +0300

    Update AppHydrationFallback.jsx

--- src/components/common/AppHydrationFallback.jsx
+++ src/components/common/AppHydrationFallback.jsx
@@ -1,17 +1,7 @@
-import { useTranslation } from "react-i18next";
+import AppLayoutSkeleton from "../../layouts/AppLayoutSkeleton";
 
 const AppHydrationFallback = () => {
-  const { t } = useTranslation();
-
-  return (
-    <div
-      className="app-hydration-loading"
-      role="status"
-      aria-label={t("loading")}
-    >
-      <span aria-hidden="true" />
-    </div>
-  );
+  return <AppLayoutSkeleton />;
 };
 
 export default AppHydrationFallback;
